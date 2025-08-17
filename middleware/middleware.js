2c5d9bb28019dc4b50d90ac9c6281951d12a0b23
hanardept <hanardept@gmail.com>
2025-08-17 07:43:31 +0300

    User management WIP

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const jwksClient = require('jwks-rsa');
 
 const whoIsTheUser = (req, res, next) => {
     // The user can't just make requests with a req.userPrivilege="admin" that they inject on their own
@@ -7,25 +8,51 @@ const whoIsTheUser = (req, res, next) => {
     // case 2) they don't have an "auth-token" header --> req.userPrivilege gets changed to "public"
 
     const token = req.header("auth-token");
+    console.log(`path: ${req.path}, token: ${req.header("auth-token")}, other: ${req.header("Auth-Token")}`);
     if (!token) {
         req.userPrivilege = "public";
         return next();
     }
 
-    try {
-        const userInfo = jwt.verify(token, process.env.JWT_TOKEN_SECRET); // userInfo: { _id: ..., privilege: "admin"/"hanar" }
-        req.userId = userInfo._id;
-        req.userPrivilege = userInfo.privilege;
-        next();
-    } catch (error) {
-        res.status(400).send("Invalid token!");
+    const authConfig = {
+        domain: process.env.AUTH0_DOMAIN,
+        audience: 'http://localhost:5000'
+    };
+
+    const client = jwksClient({
+        jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+    });
+
+    // Function to get the signing key
+    function getKey(header, callback) {
+        client.getSigningKey(header.kid, (err, key) => {
+            const signingKey = key.getPublicKey();
+            callback(null, signingKey);
+        });
     }
+
+    console.log(`token: ${token}`);
+    return jwt.verify(token, getKey, {
+        audience: authConfig.audience,
+        issuer: `https://${authConfig.domain}/`,
+        algorithms: ['RS256']
+    }, (error, userInfo) => { // userInfo: { _id: ..., privilege: "admin"/"hanar" }
+        if (error) {
+            res.status(400).send("Invalid token!");
+        } else {
+            console.log(`token verification succeeded: ${JSON.stringify(userInfo)}`);
+            req.userId = userInfo._id;
+            req.userPrivilege = userInfo.privilege;
+            next();
+        }
+    });
 };
 
 const adminAccessOnly = (req, res, next) => {
     if (req.userPrivilege === "admin") {
         next();
     } else {
+        console.log(`hii`);
         res.status(401).send("You are unauthorized to access this endpoint.");
     }
 };
