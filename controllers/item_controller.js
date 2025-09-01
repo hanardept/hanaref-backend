5d71395d817acc6f4825e924d70bb41669200937
hanardept <hanardept@gmail.com>
2025-09-01 18:11:52 +0300

    Item list - add kit cat to list item display

--- controllers/item_controller.js
+++ controllers/item_controller.js
@@ -86,7 +86,7 @@ module.exports = {
                 },
                 {
                     $project:
-                        [ 'name', 'cat', '_id', 'imageLink', 'archived', 'certificationPeriodMonths' ]
+                        [ 'name', 'cat', 'kitCats', '_id', 'imageLink', 'archived', 'certificationPeriodMonths' ]
                             .filter(field => !(filteredFieldsForRole[role] ?? []).includes(field))
                             .reduce((obj, field) => ({ ...obj, [field]: 1 }) , {})
                 },
